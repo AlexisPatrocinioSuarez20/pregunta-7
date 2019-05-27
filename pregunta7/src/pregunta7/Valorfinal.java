38fabf61e79ad7322c690093c22f2db33613ed7d
ALEXIS <ALEXIS@DESKTOP-OFFQSQ7>
2019-05-26 23:04:50 -0500

    el valor final de dos numeros con ciclos for y do while

---
+++ pregunta7/src/pregunta7/Valorfinal.java
@@ -0,0 +1,20 @@
+package pregunta7;
+
+public class Valorfinal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,x=4,w=9,q;
+	x++;
+for(q=4;q<11;q++) {
+	do {
+		i=+3;
+		w=4;
+		i= x+w;
+		x= w+i;
+	} while (x<15);
+	}
+    }
+    System.out.println("x:" + x + "i:" + i);
+	}
+
